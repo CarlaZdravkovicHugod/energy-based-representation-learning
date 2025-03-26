dda2195ec138aaca27c83a6b09f9c86e67298ff1
Lucas Vilsen <lucas.vilsen@gmail.com>
2025-03-26 13:52:56 +0100

    comet data bsub

---
+++ src/bsub_scripts/comet_data.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# A100 GPU queue, there is also gpua40 and gpua10
+#BSUB -q gpua100
+
+# job name
+#BSUB -J job_name_goes_here
+
+# 4 cpus, 1 machine, 1 gpu, 24 hours (the max)
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 24:00
+
+# at least 32 GB RAM
+#BSUB -R "rusage[mem=32GB]"
+
+# stdout/stderr files for debugging (%J is substituted for job ID)
+#BSUB -o logs/my_run_%J.out
+#BSUB -e logs/my_run_%J.err
+
+# your training script here, e.g.
+# activate environment ...
+# dotenv torchvision ipython imageio neptune matplotlib seaborn scikit-image nibabel
+source .venv/bin/activate
+PYTHONPATH="." python src/train.py --config='src/config/clevr_config.yml'
