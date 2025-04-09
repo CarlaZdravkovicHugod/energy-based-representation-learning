61715776fb38610e9b4e4dc727d61a98d93c11fa
CarlaZdravkovicHugod <s224209@dtu.dk>
2025-04-09 11:13:23 +0200

    figures for data section

--- src/data_explore.py
+++ src/data_explore.py
@@ -1,51 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.decomposition import PCA
 from dataloader import MRI2D
 from config.load_config import load_config
-import torch
-
-# 1. Visualize the data
-# 2. Visualize the distribution of the data
-# 3. Histogram if relevant
-# 4. Correlation matrix within an image
+import os
+from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('font',**{'family':'serif','serif':['Helvetica']})
 
 
-config = load_config('/Users/carlahugod/Desktop/UNI/6sem/bach/energy-based-representation-learning/src/config/2DMRI_config.yml')
+config = load_config(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config','2DMRI_config.yml')))
 dataset = MRI2D(config)
+
+org_img = np.load(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'T1w_sub-0001_x-5.npy')))
+sample, _ = dataset[0]
+sample2, _ = dataset[2]
+
 
 # first top part of the image is always just a black line
 # so remove it:
-sample, _ = dataset[1]
-sample2, _ = dataset[2]
-
 # sample_wo = sample[0][28:, 12:]  # remove the first row and first columns
 # plt.imshow(sample_wo, cmap='gray')
 # plt.title(f"Sample 1 without first 12 cols and 28 rows")
 # plt.axis('off')
 # plt.show()
 
-# Visualize the data
-plt.figure(figsize=(8, 8))
+# Visualize original image compared to sample
+plt.figure(figsize=(8, 4))
 plt.subplot(1, 2, 1)
 plt.imshow(sample[0], cmap='gray')
-plt.title(f"Sample 1")
+plt.axis('off')
+plt.title('Sample from Dataloader')
+plt.subplot(1, 2, 2)
+plt.imshow(org_img, cmap='gray')
+plt.axis('off')
+plt.title('Original Image')
+plt.savefig(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'figures', 'org_vs_sample.png')), bbox_inches='tight')
+plt.show()
+
+# Visualize 2 data samples fra dataloader
+plt.figure(figsize=(8, 4))
+plt.subplot(1, 2, 1)
+plt.imshow(sample[0], cmap='gray')
+plt.title(f"Random sample 1")
 plt.axis('off')
 plt.subplot(1, 2, 2)
 plt.imshow(sample2[0], cmap='gray')
-plt.title(f"Sample 2")
+plt.title(f"Random sample 2")
 plt.axis('off')
-plt.suptitle('2D MRI samples after processing')
-plt.tight_layout()
+plt.savefig(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'figures', 'random_samples.png')), bbox_inches='tight')
 plt.show()
 
-
 # Visualize the distribution of the data
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
 plt.hist(sample[0].flatten(), bins=50, color='blue', alpha=0.7)
-plt.title(f"Distribution of pixel values in sample 1")
+plt.title(f"Distribution of pixel values in random sample")
 plt.xlabel("Pixel value")
 plt.ylabel("Frequency")
+plt.grid()
+plt.subplot(1, 2, 2)
+plt.hist(org_img.flatten(), bins=50, color='blue', alpha=0.7)
+plt.title(f"Distribution of pixel values in original image")
+plt.xlabel("Pixel value")
+plt.ylabel("Frequency")
+plt.grid()
+plt.savefig(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'figures', 'histograms_comparison.png')))
 plt.show()
 
 
